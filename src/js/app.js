b6246274040ecdec5fe4d6073486cdb40d2fd9ad
Pablo Gutierrez <pablo_gutierrez@35057.gnm.int>
2020-01-29 16:33:23 +0000

    asia tracker

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,30 @@
 import * as d3B from 'd3'
 import * as topojson from 'topojson'
-import * as geo from 'd3-geo-projection'
+import * as geoProjection from 'd3-geo-projection'
 import { $ } from "./util"
 import asiaMap from '../assets/asia_china_extent.json'
 import loadJson from '../components/load-json'
 
-const d3 = Object.assign({}, d3B, topojson, geo);
+const d3 = Object.assign({}, d3B, topojson, geoProjection);
 
 const atomEl = $('.interactive-wrapper')
 
 const isMobile = window.matchMedia('(max-width: 600px)').matches;
+
+let isPreview = document.referrer && document.referrer.indexOf('gutools') > -1;
+
+if(window)
+{
+	if(window.location)
+	{
+		if(window.location.ancestorOrigins)
+		{
+			if(window.location.ancestorOrigins['0'].indexOf('gutools') > -1)isPreview = true;
+			
+		}
+	}
+	
+}
 
 let width = atomEl.getBoundingClientRect().width;
 let height =  isMobile ? width : width * 3.5 / 5;
@@ -30,27 +45,21 @@ const asiaChinaExtent = topojson.feature(asiaMap, {
 	geometries: asiaMap.objects.asia_china_extent.geometries.filter(d => d.properties.NAME_0 == 'China' || d.properties.code == 'JPN' || d.properties.code == 'MYS')
 });
 
-const chinaCentroid = path.centroid(asiaChinaExtent)
-const chinaLatLon = projection.invert(chinaCentroid)
-
-
-/*projection.center(chinaLatLon)
-.translate([width/2, height/2])
-.scale(isMobile ? 200 : 500)*/
-
-
-
-
 const map = d3.select('.interactive-wrapper')
 .append('svg')
 .attr('width', width)
 .attr('height', height);
 
 
-projection.fitExtent([[0, 20], [width, height - 20]], asiaChinaExtent);
+const geo = map.append('g');
+const bubbles = map.append('g');
+const labels = map.append('g');
 
 
-map.selectAll('path')
+projection.fitExtent([[0, 10], [width, height - 20]], asiaChinaExtent);
+
+
+geo.selectAll('path')
 .data(topojson.feature(asiaMap, asiaMap.objects.asia_china_extent).features.filter(d => d.properties.code != 'extent'))
 .enter()
 .append('path')
@@ -58,39 +67,71 @@ map.selectAll('path')
 .attr('class', d => 'country ' + d.properties.code.split(' ').join(''))
 
 
-const parseData = (data) => {
+const parseData = (sheet) => {
 
-	let max = d3.max(data.data, d => +d.cases );
+	d3.select('.headline').html(sheet.furniture[0].text)
+	d3.select('.timestamp').html(sheet.furniture[2].text)
+	d3.select('.source').html(sheet.furniture[1].text)
+
+	let data;
+
+	if (isPreview) {
+		data = sheet.preview;
+
+
+		console.log("=================== PREVIEW VERSION ==================")
+
+
+	} else {
+		data = sheet.data;
+
+
+		console.log("=================== LIVE VERSION ==================")
+	}
+
+
+	let max = d3.max(data, d => +d.cases );
 
 	radius.domain([0, max])
 
-	data.data.map(d => {
+	data.map(d => {
 
 		let area = d3.select('.' + d.code.split(' ').join(''))
 		.classed(' selected', true);
 
 		let feature = topojson.feature(asiaMap, asiaMap.objects.asia_china_extent).features.find(c => c.properties.code.split(' ').join('') === d.code.split(' ').join(''))
 
-		let centroid = path.centroid(feature)
+		let centroid = path.centroid(feature);
+		if(d.lat) centroid = projection([d.lon, d.lat]);
 
-		//console.log(feature, centroid, d.code)
-		
-/*		data.style.filter(s => s.feature === "selected-country").map(s => {
+		bubbles
+		.append('circle')
+		.attr("class", "bubble")
+		.attr("r", radius(+d.cases))
+		.attr("cx", centroid[0])
+		.attr("cy", centroid[1])
 
-			area
-			.style(s.style, s.value);
-
-		})*/
-
-		if(centroid[0])
+		if(d.display == 'block')
 		{
-			map
-			.append('circle')
-			.attr("class", "bubble")
-			.attr("r", radius(+d.cases))
-			.attr("cx", centroid[0])
-			.attr("cy", centroid[1])
+			let label = labels.append('text')
+			.attr('transform', 'translate(' + centroid[0] + ',' + centroid[1] + ')')
+
+			label
+			.append("tspan")
+			.attr('class','country-label')
+			.text(d.NAME)
+			.attr('x', d.offset_horizontal || 0) 
+			.attr('y', -(d.offset_vertical) )
+
+			label
+			.append('tspan')
+			.attr('class','country-cases')
+			.text(d.text)
+			.attr('x', d.offset_horizontal || 0)
+			.attr('dy', '15' )
 		}
+
+		
 
 		
 	})
@@ -103,6 +144,6 @@ loadJson('https://interactive.guim.co.uk/docsdata-test/1MHrA0nz6HVp7aJw5_KZkE90_
 .then( fileRaw => {
 	//fillFurniture(fileRaw.sheets.furniture);
 	
-	parseData (fileRaw.sheets);
+	parseData(fileRaw.sheets);
 	//window.resize();
 })
